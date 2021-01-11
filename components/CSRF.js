01fdc4f13625a08557c6fe14ce43940e09f5c17e
Sebastian Grebe <sebigrebe@googlemail.com>
2021-01-12 00:29:43 +0100

    get csrf token

--- components/CSRF.js
+++ components/CSRF.js
@@ -2,12 +2,11 @@ import { useEffect, useState } from 'react';
 import axios from '../helpers/axios';
 
 export function CSRF() {
-    console.log("start")
     const [token, setToken] = useState(null);
     useEffect(async () => {
-        console.log("hier");
-        const csrf_token = await axios.post('csrf');
-        setToken(csrf_token);
+        const csrf_token = await axios.get('csrf');
+        if(csrf_token.data && csrf_token.data.csrf_token)
+        setToken(csrf_token.data.csrf_token);
     }, []);
     return token ? <div id="csrf_token">{token}</div> : null;
 }
--- helpers/axios.js
+++ helpers/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const customInstance = axios.create ({
+const customInstance = axios.create({
     baseURL : process.env.NEXT_PUBLIC_API_HOST,
     // withCredentials: true, @todo
     headers: {
         'Accept': 'application/json',
-        'X-Request-With': 'XMLHTTPRequest'
+        'X-Requested-With': 'XMLHTTPRequest'
     }
 });
 
