4c5643594f37d793fd2122155dd75b24dca136ce
Sebastian Grebe <sebigrebe@gmail.com>
2021-01-13 23:02:23 +0100

    wip register call and response messages

--- pages/signin.js
+++ pages/signin.js
@@ -8,14 +8,13 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Link from '../components/Link';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import FooterHero from '../components/FooterHero';
-import { useState } from 'react';
-import axios from '../helpers/axios';
-import Message from '../components/Message';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -35,43 +34,10 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-  messageContainer: {
-    marginBottom: theme.spacing(2),
-  }
 }));
 
 export default function Register() {
   const classes = useStyles();
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [password_confirmation, setPasswordConfirmation] = useState('');
-  const [messages, setMessages] = useState([]);
-
-  const onFormSubmit = e => {
-    e.preventDefault();
-    console.log(email);
-    console.log(password);
-    console.log(password_confirmation);
-    axios.post('registration', {
-      email,
-      password,
-      password_confirmation
-    }).then((response) => {
-      console.log(response);
-      // @todo check response and show message
-    }, (error) => {
-      // @todo show error
-      console.log(error);
-      let alerts = JSON.parse(JSON.stringify(messages));
-      alerts.shift();
-      alerts.push({
-        text: error.message,
-        level: 'error'
-      });
-      setMessages(alerts);
-    });
-  };
 
   return (
     <Box>
@@ -81,8 +47,8 @@ export default function Register() {
                 <CssBaseline />
                 <div className={classes.paper}>
                     <Avatar className={classes.avatar}><LockOutlinedIcon /></Avatar>
-                    <Typography component="h1" variant="h5">Register</Typography>
-                    <form className={classes.form} noValidate onSubmit={onFormSubmit}>
+                    <Typography component="h1" variant="h5">Sign in</Typography>
+                    <form className={classes.form} noValidate>
                         <TextField
                             variant="outlined"
                             margin="normal"
@@ -93,8 +59,6 @@ export default function Register() {
                             name="email"
                             autoComplete="email"
                             autoFocus
-                            value={email}
-                            onChange={e => { setEmail(e.target.value) }}
                         />
                         <TextField
                             variant="outlined"
@@ -106,21 +70,10 @@ export default function Register() {
                             type="password"
                             id="password"
                             autoComplete="current-password"
-                            value={password}
-                            onChange={e => { setPassword(e.target.value) }}
                         />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password_confirmation"
-                            label="Confirm Password"
-                            type="password"
-                            id="password_confirmation"
-                            autoComplete="current-password"
-                            value={password_confirmation}
-                            onChange={e => { setPasswordConfirmation(e.target.value) }}
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
                         />
                         <Button
                             type="submit"
@@ -129,19 +82,14 @@ export default function Register() {
                             color="primary"
                             className={classes.submit}
                         >
-                            {"Register"}
+                            {"Sign In"}
                         </Button>
-                        <div className={classes.messageContainer}>
-                          {messages.map(message => (
-                            <Message level={message.level} text={message.text} />
-                          ))}
-                        </div>
                         <Grid container>
                             <Grid item xs>
                                 <Link href="/forgot_password" variant="body2">{"Forgot password?"}</Link>
                             </Grid>
                             <Grid item>
-                                <Link href="/signin" variant="body2">{"Already have an account? Sign In"}</Link>
+                                <Link href="/register" variant="body2">{"Don't have an account? Sign Up"}</Link>
                             </Grid>
                         </Grid>
                     </form>
